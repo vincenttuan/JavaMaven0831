9e48b73daf591102d7b590a7afc5ffa42c9fb943
MB-teacher <MB-teacher@MB-207-teacher>
2019-08-31 10:17:27 +0800

    Java 8

---
+++ src/main/java/day01/PetStore.java
@@ -0,0 +1,16 @@
+package day01;
+
+public class PetStore {
+    
+    public static void main(String[] args) {
+        Puppy p1 = new Puppy();
+        p1.name = "拉不拉多";
+        Puppy p2 = new Puppy();
+        p2.name = "柴犬";
+        
+        System.out.println(p1.name);
+        System.out.println(p2.name);
+        
+    }
+    
+}
---
+++ src/main/java/day01/Puppy.java
@@ -0,0 +1,14 @@
+package day01;
+
+import java.util.Date;
+
+public class Puppy {
+    String name; // 狗名
+    Date birth; // 生日
+    
+    int getAge() { // 取得年齡
+        
+        return 0;
+    }
+    
+}
