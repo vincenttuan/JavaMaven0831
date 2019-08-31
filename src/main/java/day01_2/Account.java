849b4b7d9fc45f67fb93bbb02dedd7875b90cc13
MB-teacher <MB-teacher@MB-207-teacher>
2019-08-31 12:34:17 +0800

    Java 8

--- src/main/java/day01_2/Account.java
+++ src/main/java/day01_2/Account.java
@@ -3,23 +3,37 @@ package day01_2;
 public class Account {
     private String name;
     private int balance;
+    private int password = 1234;
     
     public void setName(String name) {
         this.name = name;
     }
     
-    public void setBalance(int money) {
-        if(money >= 0) {
-            balance = money;
+    public void setBalance(int money) { // 存提款功能
+        int diff = balance + money;
+        if(diff >= 0) {
+            balance = diff;
+            String word = (money >= 0) ? "存" : "提";
+            System.out.print("\u001B[30m");
+            System.out.printf("%s款成功, 餘額: $%d, %s款: $%d\n", word, balance, word, Math.abs(money));
+        } else {
+            System.out.print("\u001B[31m");
+            System.out.printf("存款不足, 餘額: $%d, 提款: $%d\n", balance, Math.abs(money));
         }
     }
+    
 
     public String getName() {
         return name;
     }
 
-    public int getBalance() {
-        return balance;
+    public int getBalance(int password) {
+        if(this.password == password) {
+            return balance;
+        } else {
+            System.out.println("密碼錯誤");
+            return 0;
+        }
     }
     
     
--- src/main/java/day01_2/Bank.java
+++ src/main/java/day01_2/Bank.java
@@ -5,7 +5,12 @@ public class Bank {
         Account ac1 = new Account();
         ac1.setName("John");
         ac1.setBalance(100);
+        ac1.setBalance(100);
+        ac1.setBalance(-50);
+        ac1.setBalance(100);
+        ac1.setBalance(-500);
+        ac1.setBalance(300);
         System.out.println(ac1.getName());
-        System.out.println(ac1.getBalance());
+        System.out.println(ac1.getBalance(1234));
     }
 }
