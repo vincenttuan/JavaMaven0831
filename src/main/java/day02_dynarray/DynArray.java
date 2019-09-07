a1ef719daaeae4db7b386b480d967199bc1a7c32
MB-teacher <MB-teacher@MB-207-teacher>
2019-09-07 16:08:59 +0800

    Java 8

---
+++ src/main/java/day02_dynarray/DynArray.java
@@ -0,0 +1,21 @@
+package day02_dynarray;
+
+// 動態陣列分析
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DynArray {
+    public static void main(String[] args) {
+        // 靜態陣列
+        int[] scores = {100, 90, 80};
+        for(int i=0;i<scores.length;i++) {
+            System.out.println(scores[i]);
+        }
+        // 動態陣列 + Java 8
+        List scores2 = Arrays.asList(100, 90, 80);
+        scores2.forEach(x -> System.out.println(x));
+        scores2.forEach(System.out::println); // x -> System.out.println(x)
+        
+    }
+}
