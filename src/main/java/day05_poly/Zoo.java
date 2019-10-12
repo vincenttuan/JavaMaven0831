724780dccf97dfc5354c83a1ebeba1fcc2c85098
MB-teacher <MB-teacher@MB-207-teacher>
2019-10-12 14:29:45 +0800

    Java 8

--- src/main/java/day05_poly/Zoo.java
+++ src/main/java/day05_poly/Zoo.java
@@ -1,5 +1,7 @@
 package day05_poly;
 
+import java.util.Random;
+
 public class Zoo {
     public static void main(String[] args) {
         Bird bird = new Bird();
@@ -8,10 +10,30 @@ public class Zoo {
         printAnimal(bird);
         printAnimal(lion);
         printAnimal(ostrich);
+        
+        Animal animal = getAnimal();
+        printAnimal(animal);
     }
     public static void printAnimal(Animal animal) {
         System.out.printf("%s有 %s 條腿 ", animal.getKind(), animal.getLegs());
         animal.move();
+    }
+    
+    public static Animal getAnimal() {
+        int x = new Random().nextInt(3); // 0, 1, 2
+        Animal animal = null;
+        switch(x) {
+            case 0:
+                animal = new Bird();
+                break;
+            case 1:
+                animal = new Ostrich();
+                break;
+            default:
+                animal = new Lion();
+                break;    
+        }
+        return animal;
     }
     
 }
