f299cb4a93e780528bd5cac64926b2c421f2cd17
MB-teacher <MB-teacher@MB-207-teacher>
2019-10-12 10:38:01 +0800

    Java 8

--- src/main/java/day05/foo/Son.java
+++ src/main/java/day05/foo/Son.java
@@ -3,5 +3,13 @@ package day05.foo;
 import day05.bar.Father;
 
 public class Son extends Father {
-    
+    public static void main(String[] args) {
+        Son son = new Son();
+        System.out.println(son.a);
+        System.out.println(son.b);
+        
+        Father son2 = new Son();
+        System.out.println(son2.a);
+        //System.out.println(son2.b);
+    }
 }
--- src/main/java/day05/main/Test.java
+++
@@ -1,7 +0,0 @@
-package day05.main;
-
-public class Test {
-    public static void main(String[] args) {
-        
-    }
-}
