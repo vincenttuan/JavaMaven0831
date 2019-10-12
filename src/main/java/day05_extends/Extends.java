561f4b179e64383e056606b9e1f9082dc8f96edc
MB-teacher <MB-teacher@MB-207-teacher>
2019-10-12 12:06:09 +0800

    Java 8

---
+++ src/main/java/day05_extends/Extends.java
@@ -0,0 +1,37 @@
+package day05_extends;
+
+class Employee {
+    public int salary;
+}
+
+class Manager extends Employee {
+    public int budget;
+}
+
+class Director extends Manager {
+    public int stockOptions;
+}
+
+public class Extends {
+    public static void main(String[] args) {
+        Employee employee = new Employee();
+        Manager manager = new Manager();
+        Director director = new Director();
+        // line 1
+        /*
+        Which two options fail to compile when placed at line n1 of the main method?
+        A.
+        employee.salary = 50_000;
+        B.
+        director.salary = 80_000;
+        C.
+        employee.budget = 200_000;
+        D.
+        manager.budget = 1_000_000;
+        E.
+        manager.stockOption = 500;
+        F.
+        director.stockOptions = 1_000;
+        */
+    }
+}
