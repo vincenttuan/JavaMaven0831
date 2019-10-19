a13719b6f783af53ed600ad1f25f2231ee758a19
MB-teacher <MB-teacher@MB-207-teacher>
2019-10-19 10:16:42 +0800

    Java 8

--- src/main/java/day06_interface/PetStore.java
+++ src/main/java/day06_interface/PetStore.java
@@ -3,6 +3,17 @@ package day06_interface;
 public class PetStore {
     
     public static void main(String[] args) {
-        
+        Shiba shiba = new Shiba("柴犬");
+        Chihuahua chihuahua = new Chihuahua("吉娃娃");
+        showtime(shiba);
+        showtime(chihuahua);
     }
+    
+    public static void showtime(IDog dog) {
+        System.out.printf("%s 有 %d 條腿\n", dog.getKind(), IDog.legs);
+        dog.eat();
+        dog.play();
+    }
+    
+    
 }
