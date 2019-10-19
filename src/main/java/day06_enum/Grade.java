2cd790d0340da81efc5a30b97692fad37e5ca831
MB-teacher <MB-teacher@MB-207-teacher>
2019-10-19 15:04:42 +0800

    Java 8

---
+++ src/main/java/day06_enum/Grade.java
@@ -0,0 +1,15 @@
+package day06_enum;
+
+public enum Grade {
+    小學(6), 國中(3), 高中(3), 大學(4), 五專(5);
+    
+    private int value;
+ 
+    private Grade(int value) {
+        this.value = value;
+    }
+ 
+    public int getValue() {
+        return value;
+    }
+}
---
+++ src/main/java/day06_enum/GradeDemo.java
@@ -0,0 +1,8 @@
+package day06_enum;
+
+public class GradeDemo {
+    public static void main(String[] args) {
+        Grade grade = Grade.小學;
+        System.out.println(grade.getValue());
+    }
+}
