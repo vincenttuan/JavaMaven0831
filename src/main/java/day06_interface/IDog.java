96edb59a72c90d82242f92a48a4c93d1a696cec7
MB-teacher <MB-teacher@MB-207-teacher>
2019-10-19 09:47:47 +0800

    Java 8

---
+++ src/main/java/day06_interface/IDog.java
@@ -0,0 +1,13 @@
+package day06_interface;
+
+public interface IDog {
+    // 屬性
+    int legs = 4; // public static final int legs = 4;
+    // 方法(要覆寫實作)
+    void play(); // public abstract void play();
+    void eat(); // public abstract void eat();
+    // Java 8 (預設實作)
+    default void copyright() {
+        System.out.println("巨匠電腦");
+    }
+}
