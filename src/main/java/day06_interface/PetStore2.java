461903d01dd7550309d32ae6a214433f69a8f260
MB-teacher <MB-teacher@MB-207-teacher>
2019-10-19 12:23:32 +0800

    Java 8

--- src/main/java/day06_interface/PetStore2.java
+++ src/main/java/day06_interface/PetStore2.java
@@ -7,13 +7,23 @@ public class PetStore2 {
     public static void main(String[] args) {
         Decoration d1 = new Glass("狗", 100, 20);
         Decoration d2 = new Glass("貓", 150, 15);
-        showtime(d1);
-        showtime(d2);
+        Decoration d3 = new Glass("鳥", 170, 10);
         // Java 8
-        int sum = Stream.of(d1, d2).mapToInt(d -> d.getAmount() * d.getPrice())
+        Stream.of(d1, d2, d3).forEach(PetStore2::showtime);
+        int sum = Stream.of(d1, d2, d3).mapToInt(d -> d.getAmount() * d.getPrice())
                 .peek(System.out::println)
                 .sum();
         System.out.println(sum);
+        
+        // min 數量
+        Decoration min_Decoration = Stream.of(d1, d2, d3).min((a, b) -> a.getAmount() - b.getAmount()).get();
+        System.out.printf("min 數量 %d, 品名: %s\n", min_Decoration.getAmount(), min_Decoration.getName());
+        
+        // max 成本 = 數量 * 單價
+        Decoration max_Decoration = Stream.of(d1, d2, d3).max((a, b) -> (a.getAmount()*a.getPrice()) - (b.getAmount()*b.getPrice())).get();
+        System.out.printf("max 成本 %d, 品名: %s\n", max_Decoration.getAmount()*max_Decoration.getPrice(), max_Decoration.getName());
+        
+        
     }
     
     public static void showtime(Decoration d) {
