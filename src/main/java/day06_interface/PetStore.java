b4c20463e3039b2a7d52099e41b9e9c7e537b005
MB-teacher <MB-teacher@MB-207-teacher>
2019-10-19 10:30:14 +0800

    Java 8

--- src/main/java/day06_interface/PetStore.java
+++ src/main/java/day06_interface/PetStore.java
@@ -1,12 +1,19 @@
 package day06_interface;
 
+import java.util.stream.Stream;
+
 public class PetStore {
     
     public static void main(String[] args) {
+        // Java 7
         Shiba shiba = new Shiba("柴犬");
         Chihuahua chihuahua = new Chihuahua("吉娃娃");
         showtime(shiba);
         showtime(chihuahua);
+        System.out.println("---------------------------");
+        // Java 8
+        //Stream.of(shiba, chihuahua).forEach(dog -> showtime(dog));
+        Stream.of(shiba, chihuahua).forEach(PetStore::showtime);
     }
     
     public static void showtime(IDog dog) {
