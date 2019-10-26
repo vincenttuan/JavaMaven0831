5dd85e0daf63949c497237dfbcfb217977693354
MB-teacher <MB-teacher@MB-207-teacher>
2019-10-26 10:27:04 +0800

    Java 8

--- src/main/java/day07_innerclass/FuncDemo.java
+++ src/main/java/day07_innerclass/FuncDemo.java
@@ -2,6 +2,7 @@ package day07_innerclass;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class FuncDemo {
     private static List<Integer> scores;
@@ -11,10 +12,23 @@ public class FuncDemo {
     
     public static void main(String[] args) {
         
-        scores.stream().filter(t -> t >= 60).forEach(t -> System.out.println(t));
-        scores.stream().filter(t -> t >= 60).forEach(System.out::println);
+        scores.stream().filter(score -> score >= 60).forEach(score -> System.out.println(score));
+        scores.stream().filter(score -> score >= 60).forEach(System.out::println);
         
+        pass(scores, 60);
+        
+        pass2(scores, (score -> score >= 60));
+        pass2(scores, (score -> score >= 60 && score <= 90 && score % 2 == 1));
     }
+    
+    public static void pass(List<Integer> scores, int passScore) {
+        scores.stream().filter(t -> t >= passScore).forEach(System.out::println);
+    }
+    
+    public static void pass2(List<Integer> scores, Predicate<Integer> passRule) {
+        scores.stream().filter(passRule).forEach(System.out::println);
+    }
+    
     
     
 }
