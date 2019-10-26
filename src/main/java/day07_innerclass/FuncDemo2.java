1000416a808077a6747487df6a3248032615f26e
MB-teacher <MB-teacher@MB-207-teacher>
2019-10-26 10:34:23 +0800

    Java 8

---
+++ src/main/java/day07_innerclass/FuncDemo2.java
@@ -0,0 +1,19 @@
+package day07_innerclass;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class FuncDemo2 {
+    private static List<Integer> scores;
+    static {
+        scores = Arrays.asList(100, 80, 50, 40, 90);
+    }
+    
+    public static void main(String[] args) {
+        
+        
+    }
+    
+    
+}
