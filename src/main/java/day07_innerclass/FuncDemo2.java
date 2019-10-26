95b9877d78714056f7fd111578f5eeb194ec55bc
MB-teacher <MB-teacher@MB-207-teacher>
2019-10-26 10:53:27 +0800

    Java 8

--- src/main/java/day07_innerclass/FuncDemo2.java
+++ src/main/java/day07_innerclass/FuncDemo2.java
@@ -1,8 +1,10 @@
 package day07_innerclass;
 
 import java.util.Arrays;
+import java.util.DoubleSummaryStatistics;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.DoubleStream;
 
 public class FuncDemo2 {
     private static List<Integer> scores;
@@ -12,7 +14,24 @@ public class FuncDemo2 {
     
     public static void main(String[] args) {
         
+        double sum = scores.stream()
+                .mapToDouble(score -> score>=60 ? score * 1.1 : score)
+                .peek(System.out::println)
+                .sum();
+        System.out.println(sum);
         
+        double avg = scores.stream()
+                .mapToDouble(score -> score>=60 ? score * 1.1 : score)
+                .peek(System.out::println)
+                .average().getAsDouble();
+        System.out.println(avg);
+        
+        //---------------------------------------------------------------------------------
+        DoubleSummaryStatistics stat = scores.stream()
+                .mapToDouble(score -> score>=60 ? score * 1.1 : score).summaryStatistics();
+        
+        System.out.println(stat.getSum());
+        System.out.println(stat.getAverage());
     }
     
     
