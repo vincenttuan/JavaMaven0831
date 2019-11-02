cb046c199c2412a86ff1f865ba9f0cb5479a470d
MB-teacher <MB-teacher@MB-207-teacher>
2019-11-02 12:13:53 +0800

    Java 8

--- src/main/java/day08_exception/InputNumber4.java
+++ src/main/java/day08_exception/InputNumber4.java
@@ -3,21 +3,21 @@ package day08_exception;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class InputNumber3 {
+public class InputNumber4 {
     public static void main(String[] args) {
-        input();
+        try {
+            input();
+        } catch (Exception ex) {
+            System.out.println("錯了, " + ex);
+            main(null);
+        }
     }
     
     public static void input() {
         Scanner sc = new Scanner(System.in);
         System.out.println("請輸入數字(分母): ");
-        try {
-            int x = sc.nextInt();
-            System.out.printf("10 / %d = %d\n", x, 10/x);
-        } catch (Exception e) {
-            System.out.println("你錯了, " + e);
-            input();
-        }
+        int x = sc.nextInt();
+        System.out.printf("10 / %d = %d\n", x, 10/x);
     }
     
 }
