2683945256c5bc2431d34a5dee99470ac5c1e4f5
MB-Teacher <MB-Teacher@MB-207-teacher>
2019-11-09 16:43:06 +0800

    Java

--- src/main/java/day09_csv2/ReadCSV_OpenCSV2.java
+++ src/main/java/day09_csv2/ReadCSV_OpenCSV2.java
@@ -8,7 +8,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Comparator;
 
-public class ReadCSV_OpenCSV {
+public class ReadCSV_OpenCSV2 {
     public static void main(String[] args) throws Exception {
         Path path = Paths.get("src/main/java/day09_csv2/10000 Sales Records.csv");
         Reader reader = Files.newBufferedReader(path);
@@ -17,10 +17,9 @@ public class ReadCSV_OpenCSV {
                 .withIgnoreLeadingWhiteSpace(true)
                 .build();
         // 分析
-        salesList.parse().stream()
-                .filter(s -> s.getTotalProfit() >= 170_0000.0)
-                .forEach(System.out::println);
-        
-        
+        Sales maxSales = salesList.parse().stream()
+                    .max(Comparator.comparing(s -> s.getTotalProfit()))
+                    .get();
+        System.out.println(maxSales);
     }
 }
