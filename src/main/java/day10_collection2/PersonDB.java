7c0dbbfabb252071c87540e163bda6f2d21c2e49
MB-Teacher <MB-Teacher@MB-207-teacher>
2019-11-16 13:24:57 +0800

    Java

---
+++ src/main/java/day10_collection2/PersonDB.java
@@ -0,0 +1,43 @@
+package day10_collection2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PersonDB {
+
+    public static List<Person> getJavaProgrammers() {
+        List<Person> javaProgrammers = new ArrayList<Person>() {
+            {
+                add(new Person("Elsdon", "Jaycob", "Java programmer", "male", 43, 2000));
+                add(new Person("Tamsen", "Brittany", "Java programmer", "female", 23, 1500));
+                add(new Person("Floyd", "Donny", "Java programmer", "male", 33, 1800));
+                add(new Person("Sindy", "Jonie", "Java programmer", "female", 32, 1600));
+                add(new Person("Vere", "Hervey", "Java programmer", "male", 22, 1200));
+                add(new Person("Maude", "Jaimie", "Java programmer", "female", 27, 1900));
+                add(new Person("Shawn", "Randall", "Java programmer", "male", 30, 2300));
+                add(new Person("Jayden", "Corrina", "Java programmer", "female", 35, 1700));
+                add(new Person("Palmer", "Dene", "Java programmer", "male", 33, 2000));
+                add(new Person("Addison", "Pam", "Java programmer", "female", 34, 1300));
+            }
+        };
+        return javaProgrammers;
+    }
+
+    public static List<Person> getPHPProgrammers() {
+        List<Person> phpProgrammers = new ArrayList<Person>() {
+            {
+                add(new Person("Jarrod", "Pace", "PHP programmer", "male", 34, 1550));
+                add(new Person("Clarette", "Cicely", "PHP programmer", "female", 23, 1200));
+                add(new Person("Victor", "Channing", "PHP programmer", "male", 32, 1600));
+                add(new Person("Tori", "Sheryl", "PHP programmer", "female", 21, 1000));
+                add(new Person("Osborne", "Shad", "PHP programmer", "male", 32, 1100));
+                add(new Person("Rosalind", "Layla", "PHP programmer", "female", 25, 1300));
+                add(new Person("Fraser", "Hewie", "PHP programmer", "male", 36, 1100));
+                add(new Person("Quinn", "Tamara", "PHP programmer", "female", 21, 1000));
+                add(new Person("Alvin", "Lance", "PHP programmer", "male", 38, 1600));
+                add(new Person("Evonne", "Shari", "PHP programmer", "female", 40, 1800));
+            }
+        };
+        return phpProgrammers;
+    }
+}
