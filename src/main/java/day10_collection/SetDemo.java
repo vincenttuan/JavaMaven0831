8c0e947a68fadd2821b19c646cd4fdc1416ab0dd
MB-Teacher <MB-Teacher@MB-207-teacher>
2019-11-16 11:52:16 +0800

    Java

--- src/main/java/day10_collection/SetDemo.java
+++ src/main/java/day10_collection/SetDemo.java
@@ -2,6 +2,7 @@ package day10_collection;
 
 import java.util.HashSet;
 import java.util.IntSummaryStatistics;
+import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -11,12 +12,20 @@ public class SetDemo {
         set1.add(100);
         set1.add(80);
         set1.add(90);
-        Set<Integer> set2 = new TreeSet<>(); // 自然排序
+        
+        Set<Integer> set2 = new LinkedHashSet<>(); // 有序
         set2.add(100);
         set2.add(80);
         set2.add(90);
-        System.out.println(set1);
-        System.out.println(set2);
+        
+        Set<Integer> set3 = new TreeSet<>(); // 自然排序
+        set3.add(100);
+        set3.add(80);
+        set3.add(90);
+        
+        System.out.println("HashSet(無序): " + set1);
+        System.out.println("LinkedHashSet(有序): " + set2);
+        System.out.println("TreeSet(自然排序): " + set3);
         
         IntSummaryStatistics stat = set1.stream().mapToInt(x -> x).summaryStatistics();
         System.out.println(stat.getAverage());
