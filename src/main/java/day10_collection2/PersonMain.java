dc38bc095a4861bfbab5c5c44b5278899fb2c7f6
MB-Teacher <MB-Teacher@MB-207-teacher>
2019-11-16 13:51:44 +0800

    Java

--- src/main/java/day10_collection2/PersonMain.java
+++ src/main/java/day10_collection2/PersonMain.java
@@ -11,7 +11,11 @@ public class PersonMain {
         javaProgrammers.forEach(e -> System.out.print(e));
         System.out.println("\nphp Programmers: ");
         phpProgrammers.forEach(System.out::print);
-        
+        System.out.println();
+        // Java 與 Php 各別薪資總和 ?
+        int javaSalary = javaProgrammers.stream().mapToInt(x -> x.getSalary()).sum();
+        int phpSalary = phpProgrammers.stream().mapToInt(x -> x.getSalary()).sum();
+        System.out.printf("%,d, %,d, %s 多\n", javaSalary, phpSalary, (javaSalary > phpSalary)?"Java":"php");
         
     }
 }
