e9a325f92780974e464406547f5b137066109a18
MB-Teacher <MB-Teacher@MB-207-teacher>
2019-11-30 09:48:04 +0800

    SpringMVC

--- src/main/java/day11_thread2/Play.java
+++ src/main/java/day11_thread2/Play.java
@@ -6,6 +6,10 @@ public class Play {
         Running r1 = new Running();
         
         Thread t1 = new Thread(r1, "小明");
+        Thread t2 = new Thread(w1, "Mary");
+        t1.setPriority(10);
+        t2.setPriority(1);
         t1.start();
+        t2.start();
     }
 }
