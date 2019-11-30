00b7d42560193051ce2c94fde49e8e7b8323908d
MB-Teacher <MB-Teacher@MB-207-teacher>
2019-11-30 14:28:31 +0800

    Java

--- src/main/java/day12_synchronized/Seat.java
+++ src/main/java/day12_synchronized/Seat.java
@@ -7,6 +7,9 @@ public class Seat {
         String tName = Thread.currentThread().getName();
         System.out.printf("%s 去買票\n", tName);
         for(int i=0;i<seat.length;i++) {
+            
+            for (int j = 0; j < 10; j++); // 模擬延遲時間
+            
             if(!seat[i]) {
                 System.out.printf("%s 買到票 座位: %d\n", tName, i);
                 seat[i] = true;
