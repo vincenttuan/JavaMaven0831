cd454fdbbb6ae79419ac40bce2ed6f82825febf5
MB-Teacher <MB-Teacher@MB-207-teacher>
2019-11-30 16:25:34 +0800

    Java

--- src/main/java/day12_cyclicbarrier/Play.java
+++ src/main/java/day12_cyclicbarrier/Play.java
@@ -6,7 +6,9 @@ import java.util.stream.IntStream;
 public class Play {
     public static void main(String[] args) {
         int n = 4;
-        CyclicBarrier cb  = new CyclicBarrier(4);
+        Runnable r = () -> System.out.println("我們都到齊了, 買茶葉蛋~");
+        
+        CyclicBarrier cb  = new CyclicBarrier(4, r);
         IntStream.rangeClosed(1, n).forEach(i -> new Car(cb, i).start());
     }
 }
