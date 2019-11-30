a44b46ceb3c3f66e00822d073b8f890425cf7ed0
MB-Teacher <MB-Teacher@MB-207-teacher>
2019-11-30 14:40:28 +0800

    Java

--- src/main/java/day12_synchronized/Seat.java
+++ src/main/java/day12_synchronized/Seat.java
@@ -1,18 +1,20 @@
 package day12_synchronized;
 
 public class Seat {
-    private boolean[] seat = new boolean[10];
+    private boolean[] seat = new boolean[1000];
     
     public void order() {
         String tName = Thread.currentThread().getName();
         System.out.printf("%s 去買票\n", tName);
         for(int i=0;i<seat.length;i++) {
             
-            for (int j = 0; j < 10; j++); // 模擬延遲時間
+            for (int j = 0; j < 10_0000; j++); // 模擬延遲時間
             
-            if(!seat[i]) {
-                System.out.printf("%s 買到票 座位: %d\n", tName, i);
-                seat[i] = true;
+            synchronized(this) {
+                if(!seat[i]) {
+                    System.out.printf("%s 買到票 座位: %d\n", tName, i);
+                    seat[i] = true;
+                }
             }
         }
     }
