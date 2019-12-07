b3c1efafda095d2bd28100a604e1dc2ee1bd65f8
MB-Teacher <MB-Teacher@MB-207-teacher>
2019-12-07 13:49:33 +0800

    Java

---
+++ src/main/java/day13_forkjoin/Fib.java
@@ -0,0 +1,12 @@
+package day13_forkjoin;
+
+import java.util.stream.Stream;
+
+public class Fib {
+    public static void main(String[] args) {
+        Stream.iterate(new int[]{0, 1}, s -> new int[]{s[1], s[0]+s[1]})
+                .limit(10)
+                .forEach(s -> System.out.printf("%d\n", s[0]));
+        
+    }
+}
